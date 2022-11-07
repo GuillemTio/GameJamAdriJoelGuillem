4f9bea9e27fc1c2cfc4107e0e251568f0b30781e
GuillemTio <gtio@edu.tecnocampus.cat>
2022-11-07 16:25:50 +0100

    Player parte 1
    dale adri

--- main.lua
+++ main.lua
@@ -5,13 +5,13 @@ actorList = {}  --Lista de elementos de juego
 local STI = require("src/sti")
 
 function love.load()
-  --local p = Player()
-  --table.insert(actorList,p)
+  local p = Player()
+  table.insert(actorList,p)
 
   Map = STI("src/map/Map1.lua", {"box2d"})
   World = love.physics.newWorld(0, 0) -- takes x and y velocity for the World, for example to create gravity
   Map:box2d_init(World)
-  Map.layers.solid.visible = false -- colliders non visible
+  Map.layers.solid.visible = true -- colliders non visible
   --background = love.graphics.newImage("textures/background") -- this is for our future background
 
 end
@@ -20,6 +20,7 @@ function love.update(dt)
   --for _,v in ipairs(actorList) do
     --v:update(dt)
   --end
+  Player:update(dt)
   World:update(dt)
 end
 
@@ -34,6 +35,9 @@ function love.draw()
   -- anything drawn before the push and after the pop will be not affected by the scaling sti
   love.graphics.push() -- save the current transformations (positions, rotation, sacle....) to the stack
   love.graphics.scale(2,2)
+
+  Player:draw()
+
   love.graphics.pop() -- retrieves the information from the stack and resets everything to this state
   
 end
--- src/Player.lua
+++ src/Player.lua
@@ -3,7 +3,17 @@ local Player = Actor:extend()
 local Vector = Vector or require"src/vector"
 
 function Player:new(x,y)
-    Player.super.new(self,"src/textures/ninja.png",400,500,20,1,0)
+    Player.super.new(self,"src/textures/PackNinja/IndividualSprites/adventurer-idle-00",400,500,20,1,0)
+
+    self.x = 100
+    self.y = 0
+    self.width = 50
+    self.height = 37
+    
+    self.physics = {}
+    self.physics.body = love.physics.newBody(World,self.x,self.y,"dynamic")
+    self.physics.body:setFixedRotation(true)
+    self.physics.shape = love.physics.newRectangleShape(self.width/2,self.height)
 end
 
 function Player:update(dt)
