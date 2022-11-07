b03abd3ec75ffd85a64dfcf57cd9e0aba5ad25fe
GuillemTio <gtio@edu.tecnocampus.cat>
2022-11-07 20:14:48 +0100

    Player Movement

--- main.lua
+++ main.lua
@@ -7,6 +7,7 @@ local STI = require("src/sti")
 function love.load()
   Map = STI("src/map/Map1.lua", {"box2d"})
   World = love.physics.newWorld(0, 0) -- takes x and y velocity for the World, for example to create gravity
+  World:setCallbacks(beginContact, endContact)
   Map:box2d_init(World)
   Map.layers.solid.visible = false -- colliders non visible
   --background = love.graphics.newImage("textures/background") -- this is for our future background
@@ -46,4 +47,13 @@ function love.keypressed(key)
   --for _,v in ipairs(actorList) do
     
   --end
+	Player:jump(key)
+end
+
+function beginContact(a, b, collision)
+	Player:beginContact(a, b, collision)
+end
+
+function endContact(a, b, collision)
+	Player:endContact(a, b, collision)
 end
--- src/Player.lua
+++ src/Player.lua
@@ -14,24 +14,109 @@ function Player:new()
     self.xVel = 0
     self.yVel = 100
     self.maxSpeed = 200
-    self.acceleration = 4000
+    self.acceleration = 3500
     self.friction = 3000
+    self.gravity = 1500
+    self.jumpAmount = -500
+
+    self.grounded = false
     
     self.physics = {}
     self.physics.body = love.physics.newBody(World, self.x, self.y, "dynamic")
     self.physics.body:setFixedRotation(true)
-    self.physics.shape = love.physics.newRectangleShape(self.width/2,self.height) 
+    self.physics.shape = love.physics.newRectangleShape(self.width,self.height) 
     self.physics.fixture = love.physics.newFixture(self.physics.body, self.physics.shape)
 end
 
 function Player:update(dt)
     --Player.super.update(self,dt)
     self:syncPhysics()
+    self:move(dt)
+    self:applyGravity(dt)
 end
+
+function Player:applyGravity(dt)
+    if not self.grounded then
+       self.yVel = self.yVel + self.gravity * dt
+    end
+ end
+
+function Player:move(dt)
+    if love.keyboard.isDown("d", "right") then
+        if self.xVel < self.maxSpeed then
+           if self.xVel + self.acceleration * dt < self.maxSpeed then
+              self.xVel = self.xVel + self.acceleration * dt
+           else
+              self.xVel = self.maxSpeed
+           end
+        end
+     elseif love.keyboard.isDown("a", "left") then
+        if self.xVel > -self.maxSpeed then
+           if self.xVel - self.acceleration * dt > -self.maxSpeed then
+              self.xVel = self.xVel - self.acceleration * dt
+           else
+              self.xVel = -self.maxSpeed
+           end
+        end
+     else
+        self:applyFriction(dt)
+     end
+end
+
+function Player:applyFriction(dt)
+    if self.xVel > 0 then
+       if self.xVel - self.friction * dt > 0 then
+          self.xVel = self.xVel - self.friction * dt
+       else
+          self.xVel = 0
+       end
+    elseif self.xVel < 0 then
+       if self.xVel + self.friction * dt < 0 then
+          self.xVel = self.xVel + self.friction * dt
+       else
+          self.xVel = 0
+       end
+    end
+ end
 
 function Player:syncPhysics()
     self.x, self.y = self.physics.body:getPosition()
     self.physics.body:setLinearVelocity(self.xVel,self.yVel)
+end
+
+function Player:beginContact(a, b, collision)
+    if self.grounded == true then return end
+    local nx, ny = collision:getNormal()
+    if a == self.physics.fixture then
+       if ny > 0 then
+          self:land(collision)
+       end
+    elseif b == self.physics.fixture then
+       if ny < 0 then
+          self:land(collision)
+       end
+    end
+end
+
+function Player:land(collision)
+    self.currentGroundCollision = collision
+    self.yVel = 0
+    self.grounded = true
+end
+
+function Player:jump(key)
+    if (key == "space") and self.grounded then
+       self.yVel = self.jumpAmount
+       self.grounded = false
+    end
+end
+
+function Player:endContact(a, b, collision)
+    if a == self.physics.fixture or b == self.physics.fixture then
+       if self.currentGroundCollision == collision then
+          self.grounded = false
+       end
+    end
 end
 
 function Player:draw()
@@ -45,7 +130,7 @@ function Player:draw()
     
     --love.graphics.draw(self.image,xx,yy,rr,sx,sy,ox,oy,0,0)
 
-    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+    love.graphics.rectangle("fill", self.x - self.width / 2, self.y - self.height / 2, self.width, self.height)
 end
 
   return Player
