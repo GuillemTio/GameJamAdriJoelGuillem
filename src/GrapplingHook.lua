a2346aab2d1503aa2709ad221f73a9f02a8d5c19
GuillemTio <gtio@edu.tecnocampus.cat>
2022-11-10 16:50:20 +0100

    grappling acabao

--- src/GrapplingHook.lua
+++ src/GrapplingHook.lua
@@ -12,7 +12,8 @@ function GrapplingHook:new()
 
     self.firstY = self.y
     self.currentYDistance = 0
-    self.maxYDistance = 150
+    self.maxYDistanceToGrab = 180
+    self.distanceToLetGo = 50
 
     self.collided = false
 
@@ -28,14 +29,15 @@ function GrapplingHook:update(dt)
 
     if self.collided then
         self:oncollision()
+        self:checkdistancefromplayer(true)
     else
         self:syncPhysics()
-        self:checkdistance()
+        self:checkdistancefromplayer(false)
     end
 
     -- que se mueva en direccion (1,-1) CHECK
     -- a la que colisiona con algo, se queda quieto, y el jugador va hacia el gancho CHECK
-    -- si en x distancia no ha colisionado con nada se destruye y Player.grappleactive = false
+    -- si en x distancia no ha colisionado con nada se destruye y Player.grappleactive = false CHECK
 
 end
 
@@ -44,14 +46,28 @@ function GrapplingHook:syncPhysics()
     self.physics.body:setLinearVelocity(self.xVel, self.yVel)
 end
 
-function GrapplingHook:checkdistance()
-    if self.currentYDistance < self.maxYDistance then
-        self.currentYDistance = -(self.y - self.firstY)
+function GrapplingHook:checkdistancefromplayer(playeriscoming)
+    if playeriscoming then
+        local actualDistanceFromPlayer = math.sqrt(((self.x - Player.x) ^ 2) + ((-(self.y - Player.y)) ^ 2))
+        if actualDistanceFromPlayer < self.distanceToLetGo then
+            Player.grappleactive = false
+            Player.grabbed = false
+            Player.grounded = false
+            for _, v in ipairs(actorList) do
+                if v == self then
+                    table.remove(actorList, v)
+                end
+            end
+        end
     else
-        Player.grappleactive = false
-        for _,v in ipairs(actorList) do
-            if v == self then
-                table.remove(actorList,v)
+        if self.currentYDistance < self.maxYDistanceToGrab then
+            self.currentYDistance = -(self.y - self.firstY)
+        else
+            Player.grappleactive = false
+            for _, v in ipairs(actorList) do
+                if v == self then
+                    table.remove(actorList, v)
+                end
             end
         end
     end
@@ -69,7 +85,7 @@ end
 
 function GrapplingHook:draw()
     love.graphics.rectangle("fill", self.x - self.width / 2, self.y - self.height / 2, self.width, self.height)
-    love.graphics.line( Player.x, Player.y, self.x, self.y)
+    love.graphics.line(Player.x, Player.y, self.x, self.y)
 end
 
 return GrapplingHook
--- src/Player.lua
+++ src/Player.lua
@@ -1,51 +1,51 @@
 Actor = Actor or require "src/actor"
 local Player = Actor:extend()
-local Vector = Vector or require"src/vector"
+local Vector = Vector or require "src/vector"
 --local GrapplingHook = GrapplingHook or require "src/GrapplingHook"
 
 Player = {} -- Joel(07/11): no se si esto serà lo que da el problema / Joel(07/11): esta solucionado pero el tio lo tiene puesto
 
 function Player:new()
-    --Player.super.new(self,"src/textures/PackNinja/IndividualSprites/adventurer-idle-00.png",400,500,20,1,0)
-    self.image = "src/textures/PackNinja/IndividualSprites/adventurer-idle-00.png"
-    self.x = 100
-    self.y = 0
-    self.width = 50
-    self.height = 37
-    self.xVel = 0
-    self.yVel = 100
-    self.maxSpeed = 200
-    self.acceleration = 3500
-    self.friction = 3000
-    self.gravity = 1500
-    self.jumpAmount = -500
+   --Player.super.new(self,"src/textures/PackNinja/IndividualSprites/adventurer-idle-00.png",400,500,20,1,0)
+   self.image = "src/textures/PackNinja/IndividualSprites/adventurer-idle-00.png"
+   self.x = 100
+   self.y = 0
+   self.width = 50
+   self.height = 37
+   self.xVel = 0
+   self.yVel = 100
+   self.maxSpeed = 200
+   self.acceleration = 3500
+   self.friction = 3000
+   self.gravity = 1500
+   self.jumpAmount = -500
 
-    self.grappleactive = false
-    self.grabbed = false
-    self.grounded = false
+   self.grappleactive = false
+   self.grabbed = false
+   self.grounded = false
 
-    self:loadAssets{}
-    
-    self.physics = {}
-    self.physics.body = love.physics.newBody(World, self.x, self.y, "dynamic")
-    self.physics.body:setFixedRotation(true)
-    self.physics.shape = love.physics.newRectangleShape(self.width/2,self.height)  
-    self.physics.fixture = love.physics.newFixture(self.physics.body, self.physics.shape)
+   self:loadAssets {}
+
+   self.physics = {}
+   self.physics.body = love.physics.newBody(World, self.x, self.y, "dynamic")
+   self.physics.body:setFixedRotation(true)
+   self.physics.shape = love.physics.newRectangleShape(self.width / 2, self.height)
+   self.physics.fixture = love.physics.newFixture(self.physics.body, self.physics.shape)
 end
 
 function Player:update(dt)
-    --Player.super.update(self,dt)
-    self:animate(dt)
-    self:syncPhysics()
-    self:applyGravity(dt)
+   --Player.super.update(self,dt)
+   self:animate(dt)
+   self:syncPhysics()
+   self:grapplinghook(dt)
 
-    if not self.grabbed then
+   if not self.grabbed then
       self:move(dt)
-      self:grapplinghook(dt)
       --self:applyGravity(dt)
-    else
+   else
       self:movetograpple()
-    end
+   end
+   self:applyGravity(dt)
 
 end
 
@@ -59,8 +59,8 @@ end
 
 function Player:setNewFrame()
    local anim = self.animation.run
-   if anim.current <anim.total then
-      anim.current = anim.current +1
+   if anim.current < anim.total then
+      anim.current = anim.current + 1
    else
       anim.current = 1
    end
@@ -68,15 +68,16 @@ function Player:setNewFrame()
 end
 
 function Player:loadAssets()
-   self.animation = {timer = 0, rate = 0.1}
-   self.animation.run = {total = 6, current = 1, img = {}}
-   for i=1, self.animation.run.total do
-      self.animation.run.img[i] = love.graphics.newImage("src/textures/PackNinja/IndividualSprites/Run/"..i..".png")
+   self.animation = { timer = 0, rate = 0.1 }
+   self.animation.run = { total = 6, current = 1, img = {} }
+   for i = 1, self.animation.run.total do
+      self.animation.run.img[i] = love.graphics.newImage("src/textures/PackNinja/IndividualSprites/Run/" .. i .. ".png")
    end
-   
-   self.animation.iddle = {total = 4, current = 1, img = {}}
-   for i=1, self.animation.iddle.total do
-      self.animation.iddle.img[i] = love.graphics.newImage("src/textures/PackNinja/IndividualSprites/Iddle/"..i..".png")
+
+   self.animation.iddle = { total = 4, current = 1, img = {} }
+   for i = 1, self.animation.iddle.total do
+      self.animation.iddle.img[i] = love.graphics.newImage("src/textures/PackNinja/IndividualSprites/Iddle/" .. i ..
+         ".png")
    end
 
    self.animation.draw = self.animation.iddle.img[1]
@@ -87,86 +88,86 @@ function Player:loadAssets()
 end
 
 function Player:applyGravity(dt)
-    if not self.grounded then
-       self.yVel = self.yVel + self.gravity * dt
-    end
- end
+   if not self.grounded then
+      self.yVel = self.yVel + self.gravity * dt
+   end
+end
 
 function Player:move(dt)
-    if love.keyboard.isDown("d", "right") then
-        if self.xVel < self.maxSpeed then
-           if self.xVel + self.acceleration * dt < self.maxSpeed then
-              self.xVel = self.xVel + self.acceleration * dt
-           else
-              self.xVel = self.maxSpeed
-           end
-        end
-     elseif love.keyboard.isDown("a", "left") then
-        if self.xVel > -self.maxSpeed then
-           if self.xVel - self.acceleration * dt > -self.maxSpeed then
-              self.xVel = self.xVel - self.acceleration * dt
-           else
-              self.xVel = -self.maxSpeed
-           end
-        end
-     else
-        self:applyFriction(dt)
-     end
+   if love.keyboard.isDown("d", "right") then
+      if self.xVel < self.maxSpeed then
+         if self.xVel + self.acceleration * dt < self.maxSpeed then
+            self.xVel = self.xVel + self.acceleration * dt
+         else
+            self.xVel = self.maxSpeed
+         end
+      end
+   elseif love.keyboard.isDown("a", "left") then
+      if self.xVel > -self.maxSpeed then
+         if self.xVel - self.acceleration * dt > -self.maxSpeed then
+            self.xVel = self.xVel - self.acceleration * dt
+         else
+            self.xVel = -self.maxSpeed
+         end
+      end
+   else
+      self:applyFriction(dt)
+   end
 end
 
 function Player:applyFriction(dt)
-    if self.xVel > 0 then
-       if self.xVel - self.friction * dt > 0 then
-          self.xVel = self.xVel - self.friction * dt
-       else
-          self.xVel = 0
-       end
-    elseif self.xVel < 0 then
-       if self.xVel + self.friction * dt < 0 then
-          self.xVel = self.xVel + self.friction * dt
-       else
-          self.xVel = 0
-       end
-    end
- end
+   if self.xVel > 0 then
+      if self.xVel - self.friction * dt > 0 then
+         self.xVel = self.xVel - self.friction * dt
+      else
+         self.xVel = 0
+      end
+   elseif self.xVel < 0 then
+      if self.xVel + self.friction * dt < 0 then
+         self.xVel = self.xVel + self.friction * dt
+      else
+         self.xVel = 0
+      end
+   end
+end
 
 function Player:syncPhysics()
-    self.x, self.y = self.physics.body:getPosition()
-    self.physics.body:setLinearVelocity(self.xVel,self.yVel)
+   self.x, self.y = self.physics.body:getPosition()
+   self.physics.body:setLinearVelocity(self.xVel, self.yVel)
 end
 
 function Player:movetograpple()
-   local direction = Vector.new(GrapplingHook.x - self.x, GrapplingHook.y-self.y)
+   local direction = Vector.new(GrapplingHook.x - self.x, GrapplingHook.y - self.y)
    direction:normalize()
 
-   self.xVel, self.yVel = direction.x*300,direction.y*300
+   self.xVel, self.yVel = direction.x * 350, direction.y * 350
 end
 
 function Player:beginContact(a, b, collision)
-    if self.grounded == true then return end
-    local nx, ny = collision:getNormal()
-    if a == self.physics.fixture then
-       if ny > 0 then
-          self:land(collision)
-       end
-    elseif b == self.physics.fixture then
-       if ny < 0 then
-          self:land(collision)
-       end
-    end
+   if self.grounded == true then return end
+   local nx, ny = collision:getNormal()
+   if a == self.physics.fixture then
+      if ny > 0 then
+         self:land(collision)
+      end
+   elseif b == self.physics.fixture then
+      if ny < 0 then
+         self:land(collision)
+      end
+   end
 end
 
 function Player:land(collision)
-    self.currentGroundCollision = collision
-    self.yVel = 0
-    self.grounded = true
+   self.currentGroundCollision = collision
+   self.yVel = 0
+   self.grounded = true
 end
 
 function Player:jump(key)
-    if (key == "space") and self.grounded then
-       self.yVel = self.jumpAmount
-       self.grounded = false
-    end
+   if (key == "space") and self.grounded then
+      self.yVel = self.jumpAmount
+      self.grounded = false
+   end
 end
 
 function Player:grapplinghookkey(key)
@@ -174,16 +175,16 @@ function Player:grapplinghookkey(key)
    if (key == "g") and not self.grappleactive then
       self.grappleactive = true
       g = GrapplingHook:new()
-      table.insert(actorList,g)
-   
+      table.insert(actorList, g)
+
    elseif (key == "g") and self.grappleactive then
       self.grappleactive = false
       if self.grabbed then
          self.grabbed = false
          self.grounded = false
       end
-      
-      table.remove(actorList,g)
+
+      table.remove(actorList, g)
    end
    -- si le vuelvo a dar se cancela CHECK
 end
@@ -195,30 +196,30 @@ function Player:grapplinghook(dt)
 end
 
 function Player:endContact(a, b, collision)
-    if a == self.physics.fixture or b == self.physics.fixture then
-       if self.currentGroundCollision == collision then
-          self.grounded = false
-       end
-    end
+   if a == self.physics.fixture or b == self.physics.fixture then
+      if self.currentGroundCollision == collision then
+         self.grounded = false
+      end
+   end
 end
 
 function Player:draw()
-    --local xx = self.position.x
-    --local ox = self.origin.x
-    --local yy = self.position.y
-    --local oy = self.origin.y
-    --local sx = self.scale.x/15
-    --local sy = self.scale.y/15
-    --local rr = self.rot
-    
-    --love.graphics.draw(self.image,xx,yy,rr,sx,sy,ox,oy,0,0)
+   --local xx = self.position.x
+   --local ox = self.origin.x
+   --local yy = self.position.y
+   --local oy = self.origin.y
+   --local sx = self.scale.x/15
+   --local sy = self.scale.y/15
+   --local rr = self.rot
 
-    --love.graphics.rectangle("fill", self.x - self.width / 2, self.y - self.height / 2, self.width, self.height)
-    love.graphics.draw(self.animation.draw, self.x, self.y, 0, 1, 1, self.animation.width /2, self.animation.height /2)
+   --love.graphics.draw(self.image,xx,yy,rr,sx,sy,ox,oy,0,0)
 
-    if self.grappleactive then
+   --love.graphics.rectangle("fill", self.x - self.width / 2, self.y - self.height / 2, self.width, self.height)
+   love.graphics.draw(self.animation.draw, self.x, self.y, 0, 1, 1, self.animation.width / 2, self.animation.height / 2)
+
+   if self.grappleactive then
       GrapplingHook:draw()
-    end
+   end
 end
 
-  return Player
+return Player
