50194e76948e5a106b226ee4843568baa8b870f8
AdriaLopezGonzalez <alopezg@edu.tecnocampus.cat>
2022-11-14 00:21:42 +0100

    a

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@ function love.conf(t)
   t.window.width = 1280  
   t.window.height = 720
   t.window.vsync = 0
-  t.console = true
+  t.console = false
   t.title = "Brook the Hook"
 
   t.version = "11.3" -- esto lo pone por que es para la version de LOVE para la que el juego fue hecho
